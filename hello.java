7850d6683bb7534e888a8f75d0ed4e8d84a1630c
om_mac <bigadmin@om-macs-MacBook-Pro.local>
2023-05-08 21:26:06 +0530

    Testing again

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class hello {
+  public static void main(String[] args) {
+   System.out.println("Everything works");
+   int a = 257;
+   byte b = (byte)a;
+   System.out.println(b);
+  
+  }
+}
